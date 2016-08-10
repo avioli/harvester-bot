643b4226edc7c13290c86d4c0c18f4da264511b7
Evo Stamatov <aviolito@gmail.com>
2016-08-10 10:12:44 +1000

    add createProjects and getDaily funcs to harvester

--- src/index.js
+++ src/index.js
@@ -119,10 +119,42 @@ const harvester = (email, password) => {
     .then((projects) => projects.map((o) => o && o.project).filter((o) => o))
   }
 
+  const createProject = (args) => {
+    const { client_id, name, active = true } = args || {}
+
+    if (!client_id || !name) {
+      return Promise.reject(new Error('To create a project you have to provide at minimum client and name.'))
+    }
+
+    const newProject = {
+      project: Object.assign({}, args, { active })
+    }
+
+    return new Promise((resolve, reject) => {
+      harvest.Projects.create(newProject, (err, response) => err ? reject(err) : resolve(response))
+    })
+  }
+
+  const getDaily = (args) => {
+    return new Promise((resolve, reject) => {
+      harvest.TimeTracking.daily(args || {}, (err, timers) => err ? reject(err) : resolve(timers))
+    })
+  }
+
+  // TODO:
+  //   - start/stop/toggle timers
+
+  // const projectsList = promisify(harvest.Projects.list, harvest.Projects)
+  // const projectsSource = Observable.fromPromise(projectsList({}))
+  //   .flatMap((x) => x)
+  //   .pluck('project')
+
   return {
     rawHarvest: harvest,
     getInfo,
-    getProjects
+    getProjects,
+    createProject,
+    getDaily
   }
 }
 
